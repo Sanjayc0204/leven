da3ceda1f2bbdbbb9a8cdf89f695470e2cd42afc
Sanjay Chunduru <sanjaychunduru@gmail.com>
2024-10-22 16:43:05 -0400

    Integrate backend and frontend for community page

--- app/communities/[communityId]/page.tsx
+++ app/communities/[communityId]/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import NavBar from "@/components/ui/navbar";
+import { useCommunityById } from "@/components/queries/fetchCommunityById";
+
 interface CommunityPageProps {
   params: { communityId: string };
 }
 
 export default function CommunityPage({ params }: CommunityPageProps) {
   const { communityId } = params;
+  const { isLoading, isError, data, error } = useCommunityById(
+    `${communityId}`
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error! {error.message}</div>;
+  }
+  if (data) {
+    console.log(data);
+    return <div>Yay!</div>;
+  }
   return (
     <div>
-      <h1>Community ID: {communityId}</h1>
-      {/* Use communityId to fetch data or display content */}
+      <NavBar />
+      <h1>{communityId}</h1>
     </div>
   );
 }
